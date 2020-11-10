ec064d3d21c8a50d7f25584a060a69a6f68990a2
Rick Michael <rickmikegull@icloud.com>
2020-11-10 14:34:10 -0800

    added docker mount command "./interop-client rundev" and script "./devscript.sh rundev" and client_main for init functions

--- client/interop-client.sh
+++ client/interop-client.sh
@@ -16,6 +16,12 @@ then
     docker run --net=host --interactive --tty auvsisuas/interop-client
 fi
 
+# Run the client container.
+if [ "$1" == "rundev" ]
+then
+docker run --net=host --interactive --tty --name devtest  --mount type=bind,source=/Users/rick/uci-uav/interop,target=/interop auvsisuas/interop-client /bin/bash
+fi
+
 # Pulls new images.
 if [ "$1" == "pull" ]
 then
